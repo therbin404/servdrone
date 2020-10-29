45e1c5ab52bb62b4028f3018da61b5bcded20991
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-10-30 00:13:19 +0100

    add text

--- contact.php
+++ contact.php
@@ -11,9 +11,35 @@
   <?php 
     require_once('header.html');
   ?>
-  
-  <!-- ADD CONTENT HERE -->
-
+  <div class="jumbotron"></div>
+  <section class="container">
+    <div>
+    <h1>Nous contacter</h1>
+    <div class="line"></div>
+  </div>
+    <p>Pour toute question, remarque, demande, n’hésitez pas à prendre contact avec notre service clientèle</p>
+    <div class="contactMethods">
+      <div>
+        <p>Par téléphone, au<br>
+          <span class="importantText">XX.XX.XX.XX.XX</span></p>
+      </div>
+      <div>
+        <p>Par mail, à l’adresse suivante:<br>
+          <span class="importantText">XXXXX@XXXX.XX</span></p>
+      </div>
+      <div>
+        <p>Ou directement via notre<br>
+          <span class="importantText">formulaire de contact</span></p>
+      </div>
+      <div class='textUser'>
+      <input class="searchBar" type="text" id="nom" value="Nom">
+      <input class="searchBar" type="text" id="telephone" value="Téléphone">
+      <input class="searchBarEmail" type="text" id="email" value="Adresse email">
+      <input class="searchBarRequest" type="textarea" id="demande" value="Ma demande...">
+    </div>
+    </div>
+    <input class="button" type="submit" value="Envoyer">
+  </section>
   <?php
     require_once('footer.html');
   ?>
