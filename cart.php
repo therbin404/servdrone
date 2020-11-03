110cb9387499872e5d59f64d520d40e2328e089e
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-11-03 22:01:59 +0100

    added cart

--- cart.php
+++ cart.php
@@ -11,9 +11,68 @@
   <?php 
     require_once('header.html');
   ?>
-  
-  <!-- ADD CONTENT HERE -->
-
+  <div class="jumbotron"></div>
+  <section class="container cart">
+    <div class="mainTitleContainer">
+      <div class="mainTitle">
+        <h2>Mon panier</h2>
+        <div class="line"></div>
+      </div>
+    </div>
+    <div>
+      <div class="headArticle firstLine">
+        <p>Article</p>
+      </div>
+      <div>
+        <p>Serv’Drone Deluxe</p>
+      </div>
+    </div>
+    <div>
+      <div class="headArticle firstLine">
+        <p>Description</p>
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec mi.</p>
+      </div>
+    </div>
+    <div class="lineCart firstLine">
+      <div class="unitPrice">
+        <p>Prix unitaire</p>
+      </div>
+      <div class="quantity">
+        <p>Quantité</p>
+      </div>
+      <div class="price">
+        <p>Prix</p>
+      </div>
+    </div>
+    <div class="lineCart">
+      <div class="unitPrice">
+        <p>499,00 €</p>
+      </div>
+      <div class="quantity">
+        <div class="quantityButton">
+          <p>1</p>
+        </div>
+      </div>
+      <div class="price">
+        <p>499,00 €</p>
+      </div>
+    </div>
+    <hr>
+    <div class="lineCart">
+      <div class="unitPrice"> </div>
+      <div class="quantity firstLine">Sous-total : </div>
+      <div class="price">
+        <p>499,00 €</p>
+      </div>
+    </div>
+    <div class="submit">
+      <a href="checkout.html" title="Passer au paiement">
+        <input class="submitOrder" type="submit" value="Passer la commande">
+      </a>
+    </div>
+  </section>
   <?php
     require_once('footer.html');
   ?>
